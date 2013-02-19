64cd71fd171cd1b76b111aedc94423006176f811
Andrei Coman <andrei.coman@3pillarglobal.com>
2013-02-19 17:00:06 +0200

    Add configuration file option with default to ~/tldt.ini

--- src/tldt/cli.py
+++ src/tldt/cli.py
@@ -1,20 +1,23 @@
 import argparse
+import os.path
 
 import tldt
 
 
 def main():
+    user_home = os.path.expanduser("~")
     parser = argparse.ArgumentParser(description="cacat")
     parser.add_argument("head_repo")
     parser.add_argument("head_sha")
     parser.add_argument("base_repo")
     parser.add_argument("base_sha")
+    parser.add_argument("--configuration", default=os.path.join(user_home, "tldt.ini"))
     args = parser.parse_args()
     tldt.main(head_repo=args.head_repo,
-                   head_sha=args.head_sha,
-                   base_repo=args.base_repo,
-                   base_sha=args.base_sha)
-
+              head_sha=args.head_sha,
+              base_repo=args.base_repo,
+              base_sha=args.base_sha,
+              configuration_path=args.configuration)
 
 if __name__ == '__main__':
     main()
