28518caaf95d02cdb63645b64e9f8234908dda8f
Andrei Coman <andrei.coman@3pillarglobal.com>
2013-02-19 17:00:20 +0200

    Basic workflow

--- src/tldt/tldt.py
+++ src/tldt/tldt.py
@@ -1,4 +1,61 @@
+import contextlib
+import ConfigParser
+import importlib
+import os
+import subprocess
 
 
-def main(head_repo, head_sha, base_repo, base_sha):
-    raise NotImplementedError()
+@contextlib.contextmanager
+def chdir(dirname):
+    cwd = os.getcwd()
+    os.chdir(dirname)
+    yield
+    os.chdir(cwd)
+
+
+class Project(object):
+
+    def __init__(self, head_repo, head_sha, base_repo, base_sha,
+                 configuration_path):
+        self.head_repo = head_repo
+        self.head_sha = head_sha
+        self.base_repo = base_repo
+        self.base_sha = base_sha
+        self.config = ConfigParser.ConfigParser()
+        self.config.read(configuration_path)
+        self.parsers = self.config.items("ActiveParser")
+        self.root_dir = None
+        # FOOBAR
+        self.tldt()
+
+    def checkout_code(self):
+        self.root_dir = "/path/to/code_chekout"
+
+    def setup_environment(self):
+        with chdir(self.root_dir):
+            subprocess.check_call(["build/setup_environment"])
+
+    def run_tests(self):
+        with chdir(self.root_dir):
+            subprocess.check_call(["build/run_tests"])
+
+    def run_parsers(self):
+        for parser_name, parser_module in self.parsers:
+            try:
+                module = importlib.import_module(parser_module)
+                args = dict(self.config.items("parser-%s" % parser_name))
+                module.Parser(**args)
+            except ImportError:
+                print "Could not load '%s' parsing module" % parser_name
+
+    def post_results(self):
+        pass
+
+    def tldt(self):
+        self.checkout_code()
+        #self.setup_environment()
+        #self.run_tests()
+        self.run_parsers()
+        self.post_results()
+
+main = Project
