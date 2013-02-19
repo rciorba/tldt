7ecb0adf91d3920e73482641385b6d5a327cccc3
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2013-02-19 19:20:25 +0200

    add Repo.clone_or_update

--- src/tldt/git.py
+++ src/tldt/git.py
@@ -1,4 +1,10 @@
+import os
+import os.path
+import logging
+
 import sh
+
+logger = logging.getLogger(__name__)
 
 
 class Repo(object):
@@ -14,3 +20,15 @@ class Repo(object):
 
     def fetch(self, remote):
         return self.git.fetch(remote)
+
+    def clone_or_update(self, remote):
+        if not os.path.exists(self.local):
+            os.mkdir(self.local)
+        try:
+            self.git.status()
+        except sh.ErrorReturnCode_128:
+            logging.warn("cloning %s in to %s", remote, self.local)
+            self.clone(remote)
+        else:
+            logging.warn("fetching %s in to %s", remote, self.local)
+            self.fetch(remote)
