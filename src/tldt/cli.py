60119a7ea37ebe40ffedb650b92521b77a92f8bb
Andrei Coman <andrei.coman@3pillarglobal.com>
2013-02-20 12:01:22 +0200

    Mock comments support Move configparser out of tldt

--- src/tldt/cli.py
+++ src/tldt/cli.py
@@ -1,5 +1,6 @@
 import argparse
 import os.path
+import ConfigParser
 
 import tldt
 
@@ -13,11 +14,19 @@ def main():
     parser.add_argument("base_sha")
     parser.add_argument("--configuration", default=os.path.join(user_home, "tldt.ini"))
     args = parser.parse_args()
-    tldt.main(head_repo=args.head_repo,
-              head_sha=args.head_sha,
-              base_repo=args.base_repo,
-              base_sha=args.base_sha,
-              configuration_path=args.configuration)
+    config = ConfigParser.ConfigParser()
+    config.read(args.configuration)
+    username = config.get("Auth", "username")
+    password = config.get("Auth", "password")
+
+    comment = tldt.Comment(username=username, password=password)
+    runner = tldt.Project(head_repo=args.head_repo,
+                          head_sha=args.head_sha,
+                          base_repo=args.base_repo,
+                          base_sha=args.base_sha,
+                          config=config,
+                          comment=comment)
+    runner.tldt()
 
 if __name__ == '__main__':
     main()
--- src/tldt/tldt.py
+++ src/tldt/tldt.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 import contextlib
-import ConfigParser
 import importlib
 import os
 import subprocess
@@ -16,20 +15,60 @@ def chdir(dirname):
     os.chdir(cwd)
 
 
+class Comments(object):
+
+    def __init__(self, github_user, github_password):
+
+        self.general_errors = []
+        self.general_warnings = []
+        self.line_errors = []
+        self.line_warnings = []
+
+    def load_parser_results(self, parser):
+        self.general_errors.extend(parser.general_errors)
+        self.general_warnings.extend(parser.general_warnings)
+        self.line_errors.extend(parser.line_errors)
+        self.line_warnings.extend(parser.line_warnings)
+
+    def _format_error_message(self, error):
+        return "Error: %s" % error
+
+    def _format_warning_message(self, warning):
+        return "Warning: %s" % warning
+
+    def _post_general_comment(self, comment):
+        pass
+
+    def _post_line_comment(self, comment):
+        pass
+
+    def post_comments(self):
+        print "Got to posting comments"
+        for error in self.general_errors:
+            self._post_general_comment(error)
+
+        for warning in self.general_warnings:
+            self._post_general_comment(warning)
+
+        for error in self.line_errors:
+            self._post_line_comment(error)
+
+        for warning in self.line_warnings:
+            self._post_line_comment(warning)
+
+
 class Project(object):
 
     def __init__(self, head_repo, head_sha, base_repo, base_sha,
-                 configuration_path):
+                 config, comment):
         self.head_repo = head_repo
         self.head_sha = head_sha
         self.base_repo = base_repo
         self.base_sha = base_sha
-        self.config = ConfigParser.ConfigParser()
-        self.config.read(configuration_path)
+        self.config = config
+        self.comment = comment
         self.parsers = self.config.items("ActiveParsers")
         self.repo = None
-        # FOOBAR
-        self.tldt()
 
     def checkout_code(self):
         self.repo = git.Repo(self.config.get("repo", "local"))
@@ -50,12 +89,14 @@ class Project(object):
             try:
                 module = importlib.import_module(parser_module)
                 kargs = dict(self.config.items("parser-%s" % parser_name))
-                module.Parser(**kargs)
+                parser = module.Parser(**kargs)
+                parser.analyze()
+                self.comment.load_parser_results(parser)
             except ImportError as e:
                 print "Could not load '%s' parsing module.Skipping...\n %r " % (parser_name, e)
 
     def post_results(self):
-        pass
+        self.comment.post_comments()
 
     def tldt(self):
         self.checkout_code()
@@ -63,5 +104,3 @@ class Project(object):
         self.run_tests()
         self.run_parsers()
         self.post_results()
-
-main = Project
