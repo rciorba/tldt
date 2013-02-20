14fef6427761214d801aa25d93847da2be31c89d
Radu Ciorba <radu.ciorba@threepillarglobal.com>
2013-02-20 16:22:56 +0200

    add test for diff + fix issues

--- src/tldt/diff.py
+++ src/tldt/diff.py
@@ -7,16 +7,18 @@ class Mapper(object):
         self.diff = unidiff.parser.parse_unidiff(diff_file)
         self._map = {}
         for index, file_ in enumerate(self.diff, 1):
-            self._map[file_.target_path[2:]] = file_, index
+            file_name = file_.target_file[2:].strip()  # remove "b/" and trailing "\n"
+            self._map[file_name] = file_, index
 
     def file_to_diff(self, filename, linum):
-        f, f_index = self._map.get(filename, None)
+        f, f_index = self._map.get(filename, (None, None))
         if f is None:
             return None
         diff_line = 2 + f_index * 2  # account for file lines in diff
         for hunk in f:
             diff_line += hunk.length + 1  # account for hunk range info
-            start = hunk.target_start + hunk.context
-            end = hunk.target_start + hunk.target_length - hunk.context
+            start = hunk.target_start + hunk.start_context
+            # -1 because first line of hunk is at target_start
+            end = hunk.target_start + hunk.target_length - 1 - hunk.end_context
             if start <= linum <= end:
                 return diff_line
