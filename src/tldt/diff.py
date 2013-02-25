c743e76875a9b961793494c0a06ff0781acba96b
radu <radu@nimbus.(none)>
2013-02-25 22:43:50 +0200

    wip

--- src/tldt/diff.py
+++ src/tldt/diff.py
@@ -17,13 +17,13 @@ class Mapper(object):
     def file_to_diff(self, filename, linum):
         f = self._map.get(filename, None)
         if f is None:
-            logger.info("ignoring error at %s:%s" % (filename, linum))
             return None
         diff_line = 0
+        import ipdb; ipdb.set_trace()
         for hunk in f:
-            diff_line += hunk.length + 1  # account for hunk range info
-            start = hunk.target_start + hunk.start_context
+            start = hunk.target_start
             # -1 because first line of hunk is at target_start
             end = hunk.target_start + hunk.target_length - 1 - hunk.end_context
             if start <= linum <= end:
-                return diff_line
+                return linum - hunk.target_start + diff_line + 1
+            diff_line += hunk.length + 2  # account for hunk range info
--- src/tldt/diff_test.py
+++ src/tldt/diff_test.py
@@ -4,20 +4,68 @@ import unittest
 from tldt.diff import Mapper
 
 
-DIFF = """diff --git a/docs/tldt.ini b/docs/tldt.ini
-index ce9563d..aaed168 100644
---- a/docs/tldt.ini
-+++ b/docs/tldt.ini
-@@ -9,4 +9,7 @@ repo_name = django
- xunit = tldt.parsers.xunit
+DIFF = """diff --git a/requirements.txt b/requirements.txt
+index 5dabd91..abbcf7c 100644
+--- a/requirements.txt
++++ b/requirements.txt
+@@ -1,4 +1,3 @@
+ pygithub==1.11.1
+ sh==1.08
+-lxml==3.1.0
+ pylint==0.26.0
+diff --git a/src/tldt/diff.py b/src/tldt/diff.py
+index 9a02f77..ce0f228 100644
+--- a/src/tldt/diff.py
++++ b/src/tldt/diff.py
+@@ -1,6 +1,7 @@
+ import unidiff
+ from cStringIO import StringIO
+ import logging
++import sh # unused
  
- [parser-xunit]
--file = output.xml
-\ No newline at end of file
-+file = output.xml
+ logger = logging.getLogger(__name__)
+ 
+diff --git a/src/tldt/git_test.py b/src/tldt/git_test.py
+index 797bcb6..6b71aba 100644
+--- a/src/tldt/git_test.py
++++ b/src/tldt/git_test.py
+@@ -17,6 +17,11 @@ def test_clone_and_checkout(self):
+         repo = git.Repo(self.tmp_dir)
+         repo.clone(self.tmp_repo_dir)
+ 
++    def test_fail(self):
++        # This test should always fail in order to
++        # test the tldt github pull request messaging
++        self.assertTrue(False)
 +
-+[repo]
-+local = /home/rciorba/repos/tltd_new
+     def tearDown(self):
+         shutil.rmtree(self.tmp_dir)
+ 
+diff --git a/src/tldt/tldt.py b/src/tldt/tldt.py
+index 2960771..2e511dd 100644
+--- a/src/tldt/tldt.py
++++ b/src/tldt/tldt.py
+@@ -39,7 +39,6 @@ def __init__(self, github, pull_commit, pull_request, diff_factory):
+         self.diff = diff_factory(pull_request.diff_url).text
+         self.mapper = diff.Mapper(self.diff)
+ 
+-
+     def load_parser_results(self, parser):
+         self.general_errors.extend(parser.general_errors)
+         self.general_warnings.extend(parser.general_warnings)
+@@ -121,10 +120,11 @@ def pull_request(self):
+ 
+     def checkout_code(self):
+         local_checkout = self.config.get("repo", "local")
+-        logging.info("Checking out source code from %s to %s" % (self.base_repo, local_checkout))
++        logging.info("Fetching source from code from %s to %s" % (self.base_repo, local_checkout))
+         self.repo = git.Repo(local_checkout)
+         self.repo.clone_or_update(self.base_repo)
+         self.repo.fetch(self.head_repo)
++        logging.info("Checking out source code from %s to %s" % (self.base_repo, local_checkout))
+         self.repo.checkout(self.head_sha)
+ 
+     def setup_environment(self):
 """
 
 
@@ -26,11 +74,16 @@ class TestMapper(unittest.TestCase):
         self.mapper = Mapper(DIFF)
 
     def test_mapping(self):
-        self.assertIn("docs/tldt.ini", self.mapper._map)
+        self.assertIn("requirements.txt", self.mapper._map)
 
     def test_file_to_diff(self):
-        linum = self.mapper.file_to_diff("docs/tldt.ini", 15)
-        self.assertEqual(linum, 10)
+        # import ipdb; ipdb.set_trace()
+        linum = self.mapper.file_to_diff("src/tldt/diff.py", 4)
+        self.assertEqual(linum, 4)
+        linum = self.mapper.file_to_diff("requirements.txt", 3)
+        self.assertEqual(linum, 4)
+        linum = self.mapper.file_to_diff("src/tldt/tldt.py", 124)
+        self.assertEqual(linum, 14)
 
 
 if __name__ == '__main__':
